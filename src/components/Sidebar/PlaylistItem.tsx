212461c8e973ae6775340513cb4849f3a310707c
Maanas <maanassharmav@gmail.com>
2024-12-11 23:07:10 +0530

    Sidebar Playlilst option

--- src/components/Sidebar/PlaylistItem.tsx
+++ src/components/Sidebar/PlaylistItem.tsx
@@ -1,1 +1,16 @@
 import React from "react";
+
+interface PlaylistItemProps {
+  title: string;
+}
+
+export function PlaylistItem({ title }: PlaylistItemProps) {
+  return (
+    <a
+      href="#"
+      className="block py-1 text-sm text-gray-400 hover:text-white transition"
+    >
+      {title}
+    </a>
+  );
+}
