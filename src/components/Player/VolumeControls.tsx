d3ec7e1a92634f62a9d3a5d3fde931bfae980a4b
Maanas <maanassharmav@gmail.com>
2024-12-16 20:39:08 +0530

    Added volume range slider

--- src/components/Player/VolumeControls.tsx
+++ src/components/Player/VolumeControls.tsx
@@ -1,16 +1,37 @@
 import { Volume2, Maximize2 } from "lucide-react";
+import { useState } from "react";
 
 export function VolumeControls() {
+  const [value, setValue] = useState(50);
   return (
     <div className="flex items-center gap-2 min-w-[180px] justify-end">
       <button className="text-[#b3b3b3] hover:text-white transition">
         <Volume2 size={20} />
       </button>
       <div className="w-24 group">
-        <div className="h-1 bg-[#4d4d4d] rounded-full cursor-pointer">
-          <div className="relative w-2/3 h-full bg-white rounded-full group-hover:bg-[#1db954]">
-            <div className="hidden group-hover:block absolute right-0 top-1/2 -translate-y-1/2 w-3 h-3 bg-white rounded-full shadow-lg"></div>
-          </div>
+        <div className="relative h-1 bg-[#4d4d4d] rounded-full cursor-pointer group">
+          <div
+            className="absolute h-full bg-[#ffffff] rounded-full group-hover:bg-[#1db954]"
+            style={{ width: `${value}%` }} // Adjust width based on slider value
+          ></div>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={value}
+            onChange={(e) => setValue(Number(e.target.value))} // Convert string to number
+            className="absolute w-full h-1 bg-transparent appearance-none 
+          cursor-pointer 
+          [&::-webkit-slider-runnable-track]:bg-transparent 
+          [&::-webkit-slider-thumb]:appearance-none 
+          group-hover:[&::-webkit-slider-thumb]:block 
+          group-hover:[&::-webkit-slider-thumb]:relative 
+          group-hover:[&::-webkit-slider-thumb]:w-3 
+          group-hover:[&::-webkit-slider-thumb]:h-3 
+          [&::-webkit-slider-thumb]:rounded-full 
+          [&::-webkit-slider-thumb]:bg-white 
+          [&::-webkit-slider-thumb]:shadow-lg"
+          />
         </div>
       </div>
       <button className="text-[#b3b3b3] hover:text-white transition">
