5a3dd9559797d4856671bf41f3a38a3a7fb5b2c7
Maanas <maanassharmav@gmail.com>
2024-12-18 23:59:40 +0530

    Added plaaylist

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -42,6 +42,9 @@ export function Sidebar() {
           <PlaylistItem title="Tamil Hits" />
           <PlaylistItem title="Telugu Trending" />
           <PlaylistItem title="Malayalam Classics" />
+          <PlaylistItem title="Kannada Hits" />
+          <PlaylistItem title="Marathi Mix" />
+          <PlaylistItem title="Bengali Beats" />
         </div>
       </div>
     </aside>
