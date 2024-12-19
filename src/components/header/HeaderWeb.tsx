a7c39e9c1b2f8c87632d327ddd0415d41659d86f
Maanas <maanassharmav@gmail.com>
2024-12-19 22:06:01 +0530

    added "Explore Premium" and icons

--- src/components/header/HeaderWeb.tsx
+++ src/components/header/HeaderWeb.tsx
@@ -1,26 +1,42 @@
-import { ChevronLeft, ChevronRight, Bell, User } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Bell,
+  User,
+  SmilePlus,
+  Ellipsis,
+} from "lucide-react";
 
 export function HeaderWeb() {
   return (
-    <div className="hidden lg:block fixed z-10 top-0 left-0 right-0 bg-[#181818] border-t border-[#282828] px-4 py-3">
+    <div className="hidden lg:block fixed z-10 top-0 left-0 right-0 bg-[#000000] border-t border-[#282828] px-4 py-3">
       <div className=" flex justify-between items-center mx-auto">
         <div className="flex items-center gap-2">
-          <div className="hidden md:flex gap-2">
-            <button className="bg-black/70 p-1 rounded-full hover:bg-black/90 transition">
-              <ChevronLeft className="w-7 h-7" />
+          <div className="hidden md:flex gap-1">
+            <button className="hidden md:block text-[#b3b3b3] hover:text-white ">
+              <Ellipsis className="w-5 h-5" />
             </button>
             <button className="bg-black/70 p-1 rounded-full hover:bg-black/90 transition">
-              <ChevronRight className="w-7 h-7" />
+              <ChevronLeft className="w-6 h-6" />
+            </button>
+            <button className="bg-black/70 p-1 rounded-full hover:bg-black/90 transition">
+              <ChevronRight className="w-6 h-6" />
             </button>
           </div>
         </div>
 
         <div className="flex items-center gap-2">
+          <button className="hidden md:block bg-white text-[#000000] text-sm font-medium px-4 rounded-3xl p-1">
+            Explore Premium
+          </button>
           <button className="hidden md:block text-[#b3b3b3] hover:text-white p-2">
             <Bell className="w-5 h-5" />
           </button>
-          <button className="bg-black/70 p-1 rounded-full hover:bg-black/90 transition">
-            <User className="w-6 h-6" />
+          <button className="text-[#b3b3b3] p-1 rounded-full hover:text-white transition">
+            <SmilePlus className="w-5 h-5" />
+          </button>
+          <button className="text-[#b3b3b3] p-1 rounded-full  hover:text-white transition">
+            <User className="w-5 h-5" />
           </button>
         </div>
       </div>
