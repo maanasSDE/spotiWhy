8fc97aa15ccb7ed41e56c6745cf4bdcea06b1a22
Maanas <maanassharmav@gmail.com>
2024-12-30 01:07:18 +0530

    Add navbar in the mobile view

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,26 +3,30 @@ import { Sidebar } from "../Sidebar/Sidebar";
 import { MobileNav } from "../navigation/MobileNav";
 import { NowPlaying } from "../Player/NowPlaying";
 import { HeaderWeb } from "../header/HeaderWeb";
+import { MobileCategories } from "../header/mobileHeader";
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 
 export function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="h-screen bg-black text-white">
-      <HeaderWeb />
-      <div className="flex h-full">
-        <Sidebar />
-        <main className="flex-1 relative">
-          <div className="absolute inset-0 bg-gradient-to-b from-[#1f1f1f] to-[#121212]">
-            <div className="h-full overflow-y-auto pb-24 lg:pb-28">
-              {children}
+    <div className="bg-[#1f1f1f]">
+      <MobileCategories />
+      <div className="h-screen bg-black text-white">
+        <HeaderWeb />
+        <div className="flex h-full">
+          <Sidebar />
+          <main className="flex-1 relative">
+            <div className="absolute inset-0 bg-gradient-to-b from-[#1f1f1f] to-[#121212]">
+              <div className="h-full overflow-y-auto pb-24 lg:pb-28">
+                {children}
+              </div>
             </div>
-          </div>
-        </main>
+          </main>
+        </div>
+        <MobileNav />
+        <NowPlaying />
       </div>
-      <MobileNav />
-      <NowPlaying />
     </div>
   );
 }
