d133c0ef526375c3ba65aa0774ce9069962b9b89
Maanas <maanassharmav@gmail.com>
2025-01-02 20:26:54 +0530

    Add fix for mobile library allignment

--- src/components/home/mobileLibrary.tsx
+++ src/components/home/mobileLibrary.tsx
@@ -3,14 +3,13 @@ import { Heart } from "lucide-react";
 export function MobileLibrary() {
   const playlists = [
     {
-      title: "Liked Songs",
       gradient: "bg-gradient-to-br from-indigo-500 to-purple-500",
       icon: "heart",
     },
     {
       title: "Your Top Songs 2023",
       image:
-        "https://images.unsplash.com/photo-1671226366569-22c8da6d7d65?w=300&h=300&fit=crop",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/John_Denver_1974.jpg/330px-John_Denver_1974.jpg",
     },
     {
       title: "Russian",
@@ -28,14 +27,14 @@ export function MobileLibrary() {
         "https://images.unsplash.com/photo-1459749411175-04bf5292ceea?w=300&h=300&fit=crop",
     },
     {
-      title: "Best Of Rahat Fateh Ali Khan",
+      title: "Rahat Fateh Ali Khan",
       image:
         "https://images.unsplash.com/photo-1511379938547-c1f69419868d?w=300&h=300&fit=crop",
     },
   ];
 
   return (
-    <div className="lg:hidden grid grid-cols-2 gap-2">
+    <div className="lg:hidden pt-14 grid grid-cols-2 gap-2">
       {playlists.map((playlist, index) => (
         <a
           key={index}
@@ -43,10 +42,13 @@ export function MobileLibrary() {
           className="relative rounded-lg overflow-hidden aspect-[5/1] bg-[#282828]"
         >
           {playlist.gradient ? (
-            <div className={`absolute inset-0 ${playlist.gradient}`}>
+            <div
+              className={`absolute font-bold text-base pl-10 pt-3 inset-0 ${playlist.gradient}`}
+            >
+              Liked Songs
               {playlist.icon === "heart" && (
                 <div className="absolute left-3 bottom-3">
-                  <Heart className="w-3 h-3 text-white" fill="white" />
+                  <Heart className="w-5 h-5 text-white" fill="white" />
                 </div>
               )}
             </div>
@@ -54,7 +56,7 @@ export function MobileLibrary() {
             <img
               src={playlist.image}
               alt={playlist.title}
-              className="absolute inset-0 w-full h-full object-cover opacity-60"
+              className=" absolute inset-0 w-full h-full object-cover opacity-60"
             />
           )}
           <div className="absolute inset-0 p-3 flex items-end">
