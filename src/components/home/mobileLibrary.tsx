95a68ab6b253405333eebaec59d62ed79ba96c65
Maanas <maanassharmav@gmail.com>
2025-01-02 20:46:51 +0530

    Add fix for liked song allignment

--- src/components/home/mobileLibrary.tsx
+++ src/components/home/mobileLibrary.tsx
@@ -9,17 +9,18 @@ export function MobileLibrary() {
     {
       title: "Your Top Songs 2023",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/John_Denver_1974.jpg/330px-John_Denver_1974.jpg",
+        "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=300&h=300&fit=crop",
     },
+
     {
-      title: "Russian",
+      title: "Spanish Blast",
       image:
         "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=300&h=300&fit=crop",
     },
     {
-      title: "Flirting songs",
+      title: "Country Songs",
       image:
-        "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=300&h=300&fit=crop",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/John_Denver_1974.jpg/330px-John_Denver_1974.jpg",
     },
     {
       title: "EDM",
@@ -39,16 +40,16 @@ export function MobileLibrary() {
         <a
           key={index}
           href="#"
-          className="relative rounded-lg overflow-hidden aspect-[5/1] bg-[#282828]"
+          className="relative rounded-lg overflow-hidden aspect-[4/1] bg-[#282828]"
         >
           {playlist.gradient ? (
             <div
-              className={`absolute font-bold text-base pl-10 pt-3 inset-0 ${playlist.gradient}`}
+              className={`absolute font-bold text-base pl-10 pt-5 inset-0 ${playlist.gradient}`}
             >
               Liked Songs
               {playlist.icon === "heart" && (
                 <div className="absolute left-3 bottom-3">
-                  <Heart className="w-5 h-5 text-white" fill="white" />
+                  <Heart className="pb-1 w-5 h-5 text-white" fill="white" />
                 </div>
               )}
             </div>
@@ -56,7 +57,7 @@ export function MobileLibrary() {
             <img
               src={playlist.image}
               alt={playlist.title}
-              className=" absolute inset-0 w-full h-full object-cover opacity-60"
+              className="absolute inset-0 w-full h-full object-cover opacity-60"
             />
           )}
           <div className="absolute inset-0 p-3 flex items-end">
